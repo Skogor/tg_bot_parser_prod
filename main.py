72af367fd3a0114856c2affe7f239692248eccf3
Konstantin Skogorev <skogorev.k@gmail.com>
2024-03-28 22:36:01 +0300

    добавил в main код для использования workflow_data диспетчера для прокидывания конф. данных в другие модули

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from aiogram import Bot, Dispatcher
 from config_data.config import load_config
 
 config = load_config()
 
-print(config.tg_bot.session)
+bot = Bot(config.tg_bot.token)
+dp = Dispatcher()
+
+#использование workflow_data диспетчера для прокидывания конф. данных в другие модули
+tkn = config.tg_bot.token
+tel_id = config.tg_bot.id
+tel_hash = config.tg_bot.hash
+tel_session = config.tg_bot.session
+
+dp.workflow_data.update({'my_token': tkn, 'my_id': tel_id, 'my_hash': tel_hash, 'my_session': tel_session})
+
+print(dp.workflow_data)
